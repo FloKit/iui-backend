840e51fa21c705ad5dae09c3cf99de69b36a0ef5
FloKit <f.kittel@gmx.net>
2024-02-01 22:21:36 +0100

    fix recursive cursor bug

--- backend/util.py
+++ backend/util.py
@@ -104,6 +104,7 @@ def get_summary(place_id):
     else:
         reviews = get_place_details(place_id).get('reviews', [])
         summary = generate_summary(OpenAI(), reviews)
+        cursor.execute('DELETE FROM review_summaries WHERE id = ?', (place_id,))
         cursor.execute('INSERT INTO review_summaries (id, review_summary) VALUES (?, ?)', (place_id, summary))
         conn.commit()
 
